b3a7d985636c61f0452bb2b993782860209765a9
falsofacu <facundotabarez0@gmail.com>
2023-07-04 07:22:49 -0300

    Better render in iframe

--- iframeRender.js
+++ iframeRender.js
@@ -1,3 +1,8 @@
 if (window.self !== window.top) {
-  document.body.style.backgroundColor = "transparent";
+  $(document).ready(function() {
+    $("body").css("background-color", "transparent");
+    $("#background").css("display", "none");
+    $("#quote-machine").css("backdrop-filter", "unset");
+    $("#quote-machine").css("-webkit-backdrop-filter", "unset");
+  });
 }
